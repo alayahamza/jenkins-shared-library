3ecdf0107dfdcee69f1c774db065c06b12ea95f3
alayahamza <alayahamza@gmail.com>
2022-10-09 20:34:33 +0200

    log

--- src/org/alayahamza/Git.groovy
+++ src/org/alayahamza/Git.groovy
@@ -1,6 +1,12 @@
 class Git {
 
-    def clone(String credentials, String branch, String url) {
+    def log(String str) {
+        println('********************************************************')
+        println(str)
+        println('********************************************************')
+    }
+
+    def buildPackage(String credentials, String branch, String url) {
         git credentialsId: credentials, branch: branch, url: url
     }
 

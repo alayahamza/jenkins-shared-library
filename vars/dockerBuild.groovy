02ea46f3bb7a4d5dfa7eb75b8360a07034a6ad87
alayahamza <alayahamza@gmail.com>
2022-11-27 18:57:32 +0100

    Adds dockerBuild

---
+++ vars/dockerBuild.groovy
@@ -0,0 +1,13 @@
+def call(Map config) {
+    
+    stage('docker-build') {
+        container('jenkins-slave') {
+            log('Docker build')
+            sh"""
+                pwd
+                ls -lrt
+                docker build -t "$config.tag" .
+            """
+        }
+    }
+}
